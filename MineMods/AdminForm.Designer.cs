09b9b9179af0139e4ee87ed20060e53f60d0fd7e
DarkCat09 <a.chechkenev@yandex.ru>
2020-03-04 21:09:22 +0400

    Added functional to AdminForm and Reading passwd from file

--- MineMods/AdminForm.Designer.cs
+++ MineMods/AdminForm.Designer.cs
@@ -33,7 +33,37 @@
             this.ввестиПарольToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.radioButton9 = new System.Windows.Forms.RadioButton();
+            this.radioButton10 = new System.Windows.Forms.RadioButton();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button5 = new System.Windows.Forms.Button();
             this.contextMenuStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // contextMenuStrip1
@@ -46,7 +76,7 @@
             // ввестиПарольToolStripMenuItem
             // 
             this.ввестиПарольToolStripMenuItem.Name = "ввестиПарольToolStripMenuItem";
-            this.ввестиПарольToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ввестиПарольToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
             this.ввестиПарольToolStripMenuItem.Text = "Ввести пароль";
             this.ввестиПарольToolStripMenuItem.Click += new System.EventHandler(this.ввестиПарольToolStripMenuItem_Click);
             // 
@@ -70,16 +100,296 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Location = new System.Drawing.Point(13, 42);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(671, 284);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.radioButton10);
+            this.tabPage1.Controls.Add(this.radioButton9);
+            this.tabPage1.Controls.Add(this.radioButton8);
+            this.tabPage1.Controls.Add(this.radioButton7);
+            this.tabPage1.Controls.Add(this.radioButton6);
+            this.tabPage1.Controls.Add(this.radioButton5);
+            this.tabPage1.Controls.Add(this.radioButton4);
+            this.tabPage1.Controls.Add(this.radioButton3);
+            this.tabPage1.Controls.Add(this.radioButton2);
+            this.tabPage1.Controls.Add(this.radioButton1);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(663, 258);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Установить Minecraft-лаунчер";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button5);
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Controls.Add(this.textBox3);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(663, 258);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Установить мод";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(540, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Администрирование";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(663, 258);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Авторизация";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(663, 258);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Консоль администрирования";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(7, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Выберите лаунчер";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(10, 23);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(156, 17);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.Text = "Minecraft Launcher (official)";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Checked = true;
+            this.radioButton2.Location = new System.Drawing.Point(10, 46);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(77, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "TLauncher";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(10, 69);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(78, 17);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.Text = "mLauncher";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(10, 92);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(84, 17);
+            this.radioButton4.TabIndex = 4;
+            this.radioButton4.Text = "ATLauncher";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(10, 115);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(86, 17);
+            this.radioButton5.TabIndex = 5;
+            this.radioButton5.Text = "MCLauncher";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(10, 138);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(91, 17);
+            this.radioButton6.TabIndex = 6;
+            this.radioButton6.Text = "FreeLauncher";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(10, 161);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(87, 17);
+            this.radioButton7.TabIndex = 7;
+            this.radioButton7.Text = "MRLauncher";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Location = new System.Drawing.Point(10, 184);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(68, 17);
+            this.radioButton8.TabIndex = 8;
+            this.radioButton8.Text = "CyberMC";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // radioButton9
+            // 
+            this.radioButton9.AutoSize = true;
+            this.radioButton9.Location = new System.Drawing.Point(10, 207);
+            this.radioButton9.Name = "radioButton9";
+            this.radioButton9.Size = new System.Drawing.Size(63, 17);
+            this.radioButton9.TabIndex = 9;
+            this.radioButton9.Text = "MultiMC";
+            this.radioButton9.UseVisualStyleBackColor = true;
+            // 
+            // radioButton10
+            // 
+            this.radioButton10.AutoSize = true;
+            this.radioButton10.Location = new System.Drawing.Point(10, 230);
+            this.radioButton10.Name = "radioButton10";
+            this.radioButton10.Size = new System.Drawing.Size(92, 17);
+            this.radioButton10.TabIndex = 10;
+            this.radioButton10.Text = "GID-Launcher";
+            this.radioButton10.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(572, 229);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Установить";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(10, 37);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(7, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(349, 26);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Укажите путь до папки с модами\r\n(обычно C:\\Users\\user\\AppData\\Roaming\\.minecraft\\" +
+    "mods)\r\n";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(116, 36);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 22);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Обзор...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.Description = "Выберите каталог с модами";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(10, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(202, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Выберите jar-файл модификации";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(13, 102);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(119, 101);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 22);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Обзор...";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Title = "Выберите мод";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(574, 229);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(83, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Установить";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
             // AdminForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(696, 338);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "AdminForm";
             this.Text = "AdminForm";
             this.contextMenuStrip1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -91,5 +401,32 @@
         private System.Windows.Forms.ToolStripMenuItem ввестиПарольToolStripMenuItem;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton10;
+        private System.Windows.Forms.RadioButton radioButton9;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button5;
     }
 }
--- MineMods/AdminForm.cs
+++ MineMods/AdminForm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Text;
 using System.Security.Cryptography;
 using System.Windows.Forms;
@@ -7,10 +8,17 @@ namespace MineMods
 {
     public partial class AdminForm : Form
     {
+        string modsDirectory = "C:\\Users\\" + Environment.UserName + "\\AppData\\Roaming\\.minecraft\\mods";
+        string modFilename = "";
+
         public AdminForm()
         {
             InitializeComponent();
 
+            label3.Text = "Укажите путь до папки с модами\n(обычно " + modsDirectory + ")";
+            textBox2.Text = modsDirectory;
+
+            tabControl1.Visible = false;
             textBox1.Visible = false;
         }
 
@@ -21,23 +29,48 @@ namespace MineMods
 
         private void button1_Click(object sender, EventArgs e)
         {
-            byte[] tmpSource;
-            byte[] tmpHash;
-            byte[] correctHash;
+            string correctPasswdFromFile = "";
+
+            byte[] tmpSource;  //source passwd that writed in passwdbox
+            byte[] tmpHash;    //hash of passwd that writed in passwdbox
+            byte[] correctHash;//hash of correct passwd
+
+            if (File.Exists("admin_passwd.conf"))
+            {
+                StreamReader s = new StreamReader("admin_passwd.conf");
+                correctPasswdFromFile = s.ReadLine();
+                s.Close();
+
+                correctHash = new MD5CryptoServiceProvider().ComputeHash(ASCIIEncoding.ASCII.GetBytes(correctPasswdFromFile));
+            }
+            else
+            {
+                DateTime today = DateTime.Today;
+                string defpasswd = "$changeme" + today.ToString("ddMMyyyy");
+
+                //for debug
+                _ = MessageBox.Show(defpasswd);
+
+                File.WriteAllText("admin_passwd.conf", defpasswd);
+                correctHash = new MD5CryptoServiceProvider().ComputeHash(ASCIIEncoding.ASCII.GetBytes(defpasswd));
+            }
 
             if (textBox1.Text == "")
             {
                 _ = MessageBox.Show("Введите пароль!", "Ошибка авторизации",
                                     MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                tabControl1.Visible = false;
             }
             else
             {
                 tmpSource = ASCIIEncoding.ASCII.GetBytes(textBox1.Text);
                 tmpHash = new MD5CryptoServiceProvider().ComputeHash(tmpSource);
-                correctHash = new MD5CryptoServiceProvider().ComputeHash(ASCIIEncoding.ASCII.GetBytes("#An5001@987"));
 
+                //for debug
                 _ = MessageBox.Show(ByteArrayToString(correctHash));
 
+                //comparing hash
                 bool bEqual = false;
                 if (tmpHash.Length == tmpHash.Length)
                 {
@@ -55,10 +88,17 @@ namespace MineMods
                 if (bEqual)
                 {
                     _ = MessageBox.Show("Всё правильно!");
+                    tabControl1.Visible = true;
+                }
+                else
+                {
+                    _ = MessageBox.Show("Неверный пароль!");
+                    tabControl1.Visible = false;
                 }
             }
         }
 
+        //for debug
         private string ByteArrayToString(byte[] arrInput)
         {
             int i;
@@ -70,6 +110,35 @@ namespace MineMods
             }
 
             return sOutput.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            modsDirectory = folderBrowserDialog1.SelectedPath;
+            textBox2.Text = modsDirectory;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            modFilename = openFileDialog1.FileName;
+            textBox3.Text = modFilename;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (modFilename == "")
+            {
+                _ = MessageBox.Show("Выберите jar-файл с модификацией!",
+                                    "Ошибка", MessageBoxButtons.OK,
+                                    MessageBoxIcon.Error);
+            }
+            else
+            {
+                string[] filenamearr = modFilename.Split(new char[] { '\\' });
+                File.Copy(modFilename, modsDirectory + "\\" + filenamearr[filenamearr.Length - 1]);
+            }
         }
     }
 }
