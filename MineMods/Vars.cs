e19e1295665e0dc57aa6843dab5c4888e151261a
DarkCat09 <a.chechkenev@yandex.ru>
2020-03-06 15:50:30 +0300

    1

---
+++ MineMods/Vars.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Drawing;
+
+namespace MineMods
+{
+    public static class Vars
+    {
+        public static Font MAIN_FONT;
+        public static string modsFile = "mods.txt";
+    }
+}
